6681fe09894e0955654ad6dcb225c1511c488af8
Chris Edillon <jce@redhat.com>
2024-08-16 15:41:15 -0400

    remove index from FQDNs when instance count is "1"

--- dns.tf
+++ dns.tf
@@ -6,9 +6,13 @@ resource "aws_route53_record" "single_node" {
   count = var.deploy_single_node ? var.single_node_instance_count : 0
 
   zone_id = data.aws_route53_zone.aap_zone.zone_id
-  name    = "${var.single_node_instance_name}${count.index}.${var.aws_dns_zone}"
-  type    = "A"
-  ttl     = "300"
+  name = (var.single_node_instance_count == 1 ? (
+    "${var.single_node_instance_name}.${var.aws_dns_zone}"
+    ) : (
+    "${var.single_node_instance_name}${count.index}.${var.aws_dns_zone}"
+  ))
+  type = "A"
+  ttl  = "300"
   records = [
     aws_eip.single_node[count.index].public_ip
   ]
@@ -18,9 +22,13 @@ resource "aws_route53_record" "controller" {
   count = var.deploy_single_node ? 0 : var.controller_instance_count
 
   zone_id = data.aws_route53_zone.aap_zone.zone_id
-  name    = "${var.controller_instance_name}${count.index}.${var.aws_dns_zone}"
-  type    = "A"
-  ttl     = "300"
+  name = (var.controller_instance_count == 1 ? (
+    "${var.controller_instance_name}.${var.aws_dns_zone}"
+    ) : (
+    "${var.controller_instance_name}${count.index}.${var.aws_dns_zone}"
+  ))
+  type = "A"
+  ttl  = "300"
   records = [
     aws_eip.controller[count.index].public_ip
   ]
@@ -30,9 +38,13 @@ resource "aws_route53_record" "hub" {
   count = var.deploy_single_node ? 0 : var.hub_instance_count
 
   zone_id = data.aws_route53_zone.aap_zone.zone_id
-  name    = "${var.hub_instance_name}${count.index}.${var.aws_dns_zone}"
-  type    = "A"
-  ttl     = "300"
+  name = (var.hub_instance_count == 1 ? (
+    "${var.hub_instance_name}.${var.aws_dns_zone}"
+    ) : (
+    "${var.hub_instance_name}${count.index}.${var.aws_dns_zone}"
+  ))
+  type = "A"
+  ttl  = "300"
   records = [
     aws_eip.hub[count.index].public_ip
   ]
@@ -42,9 +54,13 @@ resource "aws_route53_record" "eda" {
   count = var.deploy_single_node ? 0 : var.eda_instance_count
 
   zone_id = data.aws_route53_zone.aap_zone.zone_id
-  name    = "${var.eda_instance_name}${count.index}.${var.aws_dns_zone}"
-  type    = "A"
-  ttl     = "300"
+  name = (var.eda_instance_count == 1 ? (
+    "${var.eda_instance_name}.${var.aws_dns_zone}"
+    ) : (
+    "${var.eda_instance_name}${count.index}.${var.aws_dns_zone}"
+  ))
+  type = "A"
+  ttl  = "300"
   records = [
     aws_eip.eda[count.index].public_ip
   ]
@@ -54,9 +70,13 @@ resource "aws_route53_record" "gateway" {
   count = var.deploy_single_node ? 0 : var.gateway_instance_count
 
   zone_id = data.aws_route53_zone.aap_zone.zone_id
-  name    = "${var.gateway_instance_name}${count.index}.${var.aws_dns_zone}"
-  type    = "A"
-  ttl     = "300"
+  name = (var.gateway_instance_count == 1 ? (
+    "${var.gateway_instance_name}.${var.aws_dns_zone}"
+    ) : (
+    "${var.gateway_instance_name}${count.index}.${var.aws_dns_zone}"
+  ))
+  type = "A"
+  ttl  = "300"
   records = [
     aws_eip.gateway[count.index].public_ip
   ]
